1a46db3a8ce3e3ea72b097a57878846f5e9640b8
Galya <gufkz.tkrbyf@yandex.ru>
2020-05-20 17:13:11 +0300

    new server (working)

--- server.py
+++ server.py
@@ -17,6 +17,6 @@ while True:
         data = conn.recv(1024) #recv - метод для чтения данных, принимает в арг получаемый размер данных
         if not data:
             break
-        conn.sendall(data.upper())
+        conn.sendall(data)
         
     conn.close()
